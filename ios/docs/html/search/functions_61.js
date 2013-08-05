20917cee1d94563417c660455965ff16dc6d0f82
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-08-05 09:10:28 -0700

    Add generated docs.

---
+++ ios/docs/html/search/functions_61.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['adapter',['adapter',['../interface_s_l_adapter.html#a5a7195ff8d9ffa5a314a44e75791da97',1,'SLAdapter']]],
+  ['adapterwithurl_3a',['adapterWithURL:',['../interface_s_l_adapter.html#abbdfbfe0048dd0cfa7976c0060d81232',1,'SLAdapter']]],
+  ['additem_3aformethod_3a',['addItem:forMethod:',['../interface_s_l_r_e_s_t_contract.html#a7452533bf3aa40778e1b84fdacd92da1',1,'SLRESTContract']]],
+  ['additemsfromcontract_3a',['addItemsFromContract:',['../interface_s_l_r_e_s_t_contract.html#a86c6af82b8ccebfe5fd9b779b6621d66',1,'SLRESTContract']]]
+];
---
+++ ios/docs/html/search/functions_6f.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['objectwithparameters_3a',['objectWithParameters:',['../interface_s_l_prototype.html#af7bace29cc23368b8b4d318c4a935292',1,'SLPrototype']]],
+  ['objectwithprototype_3aparameters_3a',['objectWithPrototype:parameters:',['../interface_s_l_object.html#a7ed49e15cdb4693e98e9e98562cf575d',1,'SLObject']]]
+];
