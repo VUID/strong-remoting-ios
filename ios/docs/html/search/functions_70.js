406d4fe5822d1f95efc2d598e03594015cdcdd01
Miroslav Bajtos <miroslav@strongloop.com>
2013-11-04 14:55:35 +0100

    Updated API docs.

--- ios/docs/html/search/functions_70.js
+++ ios/docs/html/search/functions_70.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['patternformethod_3a',['patternForMethod:',['../interface_s_l_r_e_s_t_contract.html#a612649b0a4dcca206c38b3caa2598569',1,'SLRESTContract']]],
-  ['prototypewithname_3a',['prototypeWithName:',['../interface_s_l_prototype.html#ab349813e141d002a57c9c7aca22cb66f',1,'SLPrototype']]]
+  ['patternformethod_3a',['patternForMethod:',['../interface_s_l_r_e_s_t_contract.html#a612649b0a4dcca206c38b3caa2598569',1,'SLRESTContract']]]
 ];
