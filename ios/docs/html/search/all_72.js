021132c2b7ac1c93d5a65a75bfc6ffebd0aaf4c6
Miroslav Bajtos <miroslav@strongloop.com>
2013-11-25 11:52:09 +0100

    Update API docs

--- ios/docs/html/search/all_72.js
+++ ios/docs/html/search/all_72.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['repository',['repository',['../interface_s_l_object.html#a3dbcb5c6229ba41568990d25f0c0b7b6',1,'SLObject']]],
-  ['repositoryforclassname_3a',['repositoryForClassName:',['../interface_s_l_repository.html#a2295bbe3440fd28bcf339b40cebc56b0',1,'SLRepository']]]
+  ['repositorywithclassname_3a',['repositoryWithClassName:',['../interface_s_l_repository.html#a45a90e030effe9dd8c54ed9c968e441d',1,'SLRepository']]]
 ];
